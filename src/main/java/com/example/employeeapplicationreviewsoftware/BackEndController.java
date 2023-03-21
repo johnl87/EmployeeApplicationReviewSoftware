29e00b1650987f7e4d7372ce7186433aa040cd27
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-21 15:28:52 -0400

    login page

--- src/main/java/com/example/employeeapplicationreviewsoftware/BackEndController.java
+++ src/main/java/com/example/employeeapplicationreviewsoftware/BackEndController.java
@@ -3,7 +3,7 @@ package com.example.employeeapplicationreviewsoftware;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class BackEndController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/com/example/employeeapplicationreviewsoftware/Main.java
+++ src/main/java/com/example/employeeapplicationreviewsoftware/Main.java
@@ -1,17 +1,16 @@
 package com.example.employeeapplicationreviewsoftware;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         //the file is found under resources to edit in scene builder
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         //v is width, v1 is height
         Scene scene = new Scene(fxmlLoader.load(), 620, 240);
         stage.setTitle("Employee Application Review System");
