13f65d89b1e0b27f63ca8521c02c7aa4c84538c5
joel662 <87988677+joel662@users.noreply.github.com>
2023-03-28 23:19:43 -0400

    Review Page

---
+++ src/main/java/com/example/employeeapplicationreviewsoftware/Profile.java
@@ -0,0 +1,50 @@
+package com.example.employeeapplicationreviewsoftware;
+
+public class Profile {
+    private String department;
+    private String status;
+    private String name;
+    private String comment;
+    private int rating;
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    // Todo: have a button to go to the previous screen
+
+}
---
+++ src/main/java/com/example/employeeapplicationreviewsoftware/ReviewPage.java
@@ -0,0 +1,15 @@
+package com.example.employeeapplicationreviewsoftware;
+
+
+public class ReviewPage{
+    private String applicant;
+    public String getApplicant() {
+        return applicant;
+    }
+    public void setApplicant(String applicant) {
+        this.applicant = applicant;
+    }
+    //Todo on button click
+
+
+}
