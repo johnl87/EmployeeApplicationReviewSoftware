b0b984c06a5c627b5305048c45546516c9d3d7d3
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-31 00:20:51 -0400

    configured reset page to display confirmation of password reset

--- src/main/java/com/example/employeeapplicationreviewsoftware/ResetPasswordController.java
+++ src/main/java/com/example/employeeapplicationreviewsoftware/ResetPasswordController.java
@@ -6,12 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class ResetPasswordController {
+
+    @FXML
+    Button resetBtn;
+    @FXML
+    TextArea resetText;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -22,6 +30,13 @@ public class ResetPasswordController {
         scene = new Scene(root, 282, 482);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    protected void onResetButtonClick(ActionEvent event) throws IOException{
+        resetText.setText("Password has been reset." + "\n"
+                +  "Check your email for more details." + "\n"
+                + "Click back to return to login");
+
     }
 
 
