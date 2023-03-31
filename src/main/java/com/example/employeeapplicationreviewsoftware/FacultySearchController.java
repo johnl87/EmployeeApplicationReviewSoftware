924e4409fbeebf8605810e8fd70761321bd53a7a
joel662 <87988677+joel662@users.noreply.github.com>
2023-03-30 23:46:21 -0400

    Back Button in FacultySearch

--- src/main/java/com/example/employeeapplicationreviewsoftware/FacultySearchController.java
+++ src/main/java/com/example/employeeapplicationreviewsoftware/FacultySearchController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,5 +16,13 @@ public class FacultySearchController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    void backAction(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AfterLogin.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 282, 482);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
