9f6e7747a46d25da211c86bbdf4fe9339db46a8f
joel662 <87988677+joel662@users.noreply.github.com>
2023-03-30 23:41:54 -0400

    Back Button in ReviewPage

--- src/main/java/com/example/employeeapplicationreviewsoftware/ReviewPage.java
+++ src/main/java/com/example/employeeapplicationreviewsoftware/ReviewPage.java
@@ -1,7 +1,24 @@
 package com.example.employeeapplicationreviewsoftware;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class ReviewPage{
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     private String applicant;
     public String getApplicant() {
         return applicant;
@@ -9,7 +26,16 @@ public class ReviewPage{
     public void setApplicant(String applicant) {
         this.applicant = applicant;
     }
-    //Todo on button click
+    @FXML
+    private Button backButton;
 
+    @FXML
+    void backAction(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AfterLogin.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 282, 482);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
